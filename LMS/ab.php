d88e78421ac6d7caa8cad233412d07d1a5447bea
govind8534 <105936865+govind8534@users.noreply.github.com>
2023-12-03 20:08:24 +0530

    Add files via upload

---
+++ LMS/ab.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMS</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+	#main_content{
+		padding: 50px;
+		background-color: darkcyan;
+	}
+	#side_bar{
+		background-color: whitesmoke;
+		padding: 50px;
+		width: 300px;
+		height: 450px;
+		background-color:bisque;
+	}
+</style>
+<body>
+
+		</div>
+		<div class="col-md-8" id="main_content">
+			<center><h3><u>User Registration Form</u></h3></center>
+			<form action="register.php" method="post">
+				<div class="form-group">
+					<label for="name">Full Name:</label>
+					<input type="text" name="name" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="email">Email ID:</label>
+					<input type="text" name="email" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" name="password" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="mobile">Mobile:</label>
+					<input type="text" name="mobile" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="address">Address:</label>
+					<textarea name="address" class="form-control" required></textarea> 
+				</div>
+				<button type="submit" class="btn btn-primary">Register</button>	
+			</form>
+		</div>
+	</div>
+</body>
+</html>
